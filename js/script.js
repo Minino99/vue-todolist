96c1d637d3a5d30a69f01d67aa99ced342ba88d0
Minino99 <loizzo.pasqualino@gmail.com>
2022-06-13 17:52:00 +0200

    milestones done

--- js/script.js
+++ js/script.js
@@ -1,42 +1,44 @@
 let toDoList = [
- {
-  name : "Learn VueJS",
-  completed : false,
- },
-    {
-     name : "Learn JavaScript",
-     completed : false,
-    },
-    {
-     name : "Learn HTML",
-     completed : true,
-    },
-    {
-     name : "Learn CSS",
-     completed : true,
-    },
-    {
-     name : "Buy groceries",
-     completed : false,
-    }
+  {
+    name: "Learn VueJS",
+    completed: false,
+  },
+  {
+    name: "Learn JavaScript",
+    completed: false,
+  },
+  {
+    name: "Learn HTML",
+    completed: true,
+  },
+  {
+    name: "Learn CSS",
+    completed: true,
+  },
+  {
+    name: "Buy groceries",
+    completed: false,
+  },
 ];
 
 const vue = new Vue({
-    el: '#app',
-    data: {
-        toDoList: toDoList,
-        userTask: "",
-    },
-    methods: {
-     deleteTask(i) {
+  el: "#app",
+  data: {
+    toDoList: toDoList,
+    userTask: "",
+  },
+  methods: {
+    deleteTask(i) {
       this.toDoList.splice(i, 1);
-     },
-     addTask() {
-      const testo = this.userTask.trim();
-      this.toDoList.push({
-       name: testo,
-       completed: false,
-     });
     },
-    }
-   });
+    addTask() {
+      const testo = this.userTask.trim();
+      if (testo !== "") {
+        this.toDoList.push({
+          name: testo,
+          completed: false,
+        });
+      }
+    },
+  },
+});
